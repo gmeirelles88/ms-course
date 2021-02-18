68967afbd59fb92e60c4f2a73a236e2e14fa6d5b
gldl <gabriel.meirelles@gft.com>
2021-02-18 09:20:25 -0300

    Project hr-ouath created

---
+++ hr-oauth/src/test/java/com/gft/hrouath/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.hrouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
